2e6aa2f7638cf9bf0a1d9cda7bb62ed1099c0859
Laura <lahfumagalli@gmail.com>
2021-10-04 12:34:19 -0300

    exercicios finalizados

--- LOGICA/exercicio2.js
+++ LOGICA/exercicio2.js
@@ -3,7 +3,16 @@ const mercado = require('./mercado');
 // complete a função "vitaminaC",
 // para retornar um array com o nome de todas as frutas que contenham a vitamina C;
 function vitaminaC() {
-  
+  const allFruits = mercado.frutas;
+  const fruitsVitC = [];
+  for (let i = 0; i < allFruits.length; i += 1){
+    const vit = allFruits[i]['valorNutritivo'];
+    if (vit.includes('C')) {
+      fruitsVitC.push(allFruits[i]['nome']);
+    }
+  }
+  return fruitsVitC;
 }
+console.log(vitaminaC())
 
 module.exports = vitaminaC;
